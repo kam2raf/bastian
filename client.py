5d560d4864908bd25bfb2761f65b6813d941ffe7
kam2raf <kam2raf@ukr.net>
2026-07-18 23:19:03 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,26 @@
-import requests
+from sample_api import DATA
+
+from request import Request
+
+
+class ApiClient:
+
+    def send(
+
+        self,
+
+        endpoint
+
+    ):
+
+        request = Request(
+
+            endpoint
+
+        )
+
+        return DATA.get(
+
+            request.endpoint.path
+
+        )
